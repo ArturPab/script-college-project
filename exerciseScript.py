e36f2e8ac97cb80c16c8acdfc65f6b14e16a02d7
Artur Pabjan <artur.pabjan00@gmail.com>
2021-11-16 12:26:09 +0100

    cleaned up code, added handling exceptions

--- exerciseScript.py
+++ exerciseScript.py
@@ -18,27 +18,47 @@ def isSubstringCheckedProperly(substring, words):
 # Wlacza sie skrypt, gdzie pierwsza zminna to liczba liter słów, ktore skrypt ma wyszukac, druga zmienna to liczba ostatnich liter tych slow
 def runScript(wordLength, substringLength):
     # Sprawdza poprawnosc danych
-    if wordLength > substringLength:
-        dictionaryFile = open("slownik.txt", "r")
-        resultFile = open("result.txt", "w", encoding='utf-8')
+    if isDataCorrect(substringLength, wordLength):
+        try:
+            dictionaryFile = open("slownik.txt", "r")
+            resultFile = open("result.txt", "w", encoding='utf-8')
+        except FileExistsError:
+            print("Oops!  That was no valid number.  Try again...")
+        else:
+            wordsWithWordLength = []
+            wordsWithSubstringLength = {}
+            saveResult(dictionaryFile, resultFile, substringLength, wordLength, wordsWithSubstringLength,
+                       wordsWithWordLength)
+            closeFiles(dictionaryFile, resultFile)
 
-        wordsWithWordLength = []
-        wordsWithSubstringLength = {}
 
-        # Petla wykonuje sie dopoki istnieje linia w pliku
-        for word in dictionaryFile:
-            # Ucina biale znaki
-            word = word.strip()
-            # Znajduje slowa z dlugoscia zadana przez druga zmienna wejsciowa
-            if wordHaveCharsEnough(word, substringLength):
-                wordsWithSubstringLength[word] = word
-            # znajduje slowa z dlugoscia zadana przez pierwsza zmienna wejsciowa
-            elif wordHaveCharsEnough(word, wordLength):
-                wordsWithWordLength.append(word)
-        for word in wordsWithWordLength:
-            if isSubstringCheckedProperly(word[len(word) - substringLength:], wordsWithSubstringLength):
-                # Zapisuje slowo do pliku
-                resultFile.write(word + '\n')
-        # Zamyka pliki
-        resultFile.close()
-        dictionaryFile.close()
+def isDataCorrect(substringLength, wordLength):
+    return wordLength > substringLength
+
+
+# Funkcja zamyka pliki
+def closeFiles(dictionaryFile, resultFile):
+    resultFile.close()
+    dictionaryFile.close()
+
+
+# Funkcja zapisuje wyniki do pliku
+def saveResult(dictionaryFile, resultFile, substringLength, wordLength, wordsWithSubstringLength, wordsWithWordLength):
+    findWords(dictionaryFile, substringLength, wordLength, wordsWithSubstringLength, wordsWithWordLength)
+    # Petla wykonuje sie dopoki istnieje linia w pliku
+    for word in wordsWithWordLength:
+        if isSubstringCheckedProperly(word[len(word) - substringLength:], wordsWithSubstringLength):
+            # Zapisuje slowo do pliku
+            resultFile.write(word + '\n')
+
+
+def findWords(dictionaryFile, substringLength, wordLength, wordsWithSubstringLength, wordsWithWordLength):
+    for word in dictionaryFile:
+        # Ucina biale znaki
+        word = word.strip()
+        # Znajduje slowa z dlugoscia zadana przez druga zmienna wejsciowa
+        if wordHaveCharsEnough(word, substringLength):
+            wordsWithSubstringLength[word] = word
+        # znajduje slowa z dlugoscia zadana przez pierwsza zmienna wejsciowa
+        elif wordHaveCharsEnough(word, wordLength):
+            wordsWithWordLength.append(word)
